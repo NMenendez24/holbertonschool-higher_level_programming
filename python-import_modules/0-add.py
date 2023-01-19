c1e6bc8e316a7a10b3b0f0ec0dd563ee73b82d24
NMenendez24 <nahuelmt24@gmail.com>
2023-01-19 14:49:52 -0300

    Fixes

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
-b = 2
 a = 1
+b = 2
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-a = 10
 b = 5
+a = 10
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
