5b6019ca1b5366a4e419d4d54b70011e7fc7efb4
NMenendez24 <nahuelmt24@gmail.com>
2023-01-19 18:30:04 -0300

    Task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
     for i in range(0, len(names)):
-        if names[i][0] != "_" and names[i][1] != "_":
+        if names[i][0] != '_' and names[i][1] != '_':
             print(names)
