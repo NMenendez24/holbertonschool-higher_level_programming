633c475e6a86512430607b3a26bccd3d1cf8ca8b
NMenendez24 <nahuelmt24@gmail.com>
2023-01-19 19:00:17 -0300

    Task 4 Fix?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    import hidden_4
     names = dir(hidden_4)
     for i in range(0, len(names)):
-        if names[i][0] != '_' and names[i][1] != '_':
-            print(names)
+        if (names[i])[0] != '_' and (names[i])[1] != '_':
+            print(names[i])
