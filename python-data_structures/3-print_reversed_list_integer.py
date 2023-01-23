a83714ae6e4da45ef3001af7912fa44d61289fd7
NMenendez24 <nahuelmt24@gmail.com>
2023-01-23 14:43:54 -0300

    PEP8 Validation

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list == None:
+    if my_list is None:
         return
     for i in reversed(range(0, len(my_list))):
         print("{:d}".format(my_list[i]))
