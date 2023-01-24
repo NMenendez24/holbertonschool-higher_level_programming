467332ce116bde8206d9d24a7b596c92dc7e139b
NMenendez24 <nahuelmt24@gmail.com>
2023-01-24 17:19:26 -0300

    Tasks 0 and 1 Fix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = list(map(list, matrix))
-    for i in range(0, 3):
-        for j in range(0, 3):
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[0])):
             new_matrix[i][j] = new_matrix[i][j] ** 2
     return new_matrix
--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
     for i in range(0, len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
