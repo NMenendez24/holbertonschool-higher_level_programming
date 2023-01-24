86e12bf4f2b757fe85035f46a1129b713bdb4da5
NMenendez24 <nahuelmt24@gmail.com>
2023-01-24 18:03:39 -0300

    Task 3 PEP8

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    new_list=[]
+    new_list = []
     set_1_list = list(set_1)
     set_2_list = list(set_2)
     for i in range(0, len(set_1)):
