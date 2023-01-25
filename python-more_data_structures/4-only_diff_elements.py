ee630cd249f2593c20f681dd6e323f77523c281c
NMenendez24 <nahuelmt24@gmail.com>
2023-01-25 13:21:15 -0300

    Added another way to do the exercice

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -5,3 +5,14 @@ def only_diff_elements(set_1, set_2):
     set_2_list = list(set_2)
     new_list = set(set_1_list) ^ set(set_2_list)
     return new_list
+
+#Also works this way
+
+"""
+def only_diff_elements(set_1, set_2):
+    new_list = []
+    # set_1_list = list(set_1)
+    # set_2_list = list(set_2)
+    new_list = set(set_1) ^ set(set_2)
+    return new_list
+"""
