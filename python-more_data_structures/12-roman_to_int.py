431f408673133499aba65b8e6580582028c539d8
NMenendez24 <nahuelmt24@gmail.com>
2023-01-25 16:29:58 -0300

    Task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_list = list(roman_string)
+    a = 0
+    for i in range(0, len(roman_list)):
+        if roman_list[i] == 'I':
+            roman_list[i] = 1
+        if roman_list[i] == 'V':
+            roman_list[i] = 5
+        if roman_list[i] == 'X':
+            roman_list[i] = 10
+        if roman_list[i] == 'L':
+            roman_list[i] = 50
+        if roman_list[i] == 'C':
+            roman_list[i] = 100
+        if roman_list[i] == 'D':
+            roman_list[i] = 500
+        if roman_list[i] == 'M':
+            roman_list[i] = 1000
+    print(roman_list)
+    for i in range(0, len(roman_list)):
+        if i + 1 < len(roman_list):
+            if int(roman_list[i]) >= int(roman_list[i + 1]):
+                a += int(roman_list[i])
+            else:
+                a -= int(roman_list[i])
+        if i == len(roman_list) - 1:
+            a += int(roman_list[i])
+    return a
