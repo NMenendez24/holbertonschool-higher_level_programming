27735988f81afc4f93f7cc890080dffeadce94ce
NMenendez24 <nahuelmt24@gmail.com>
2023-01-25 13:16:51 -0300

    Tasks 4, 5 and 6

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    new_list = []
+    set_1_list = list(set_1)
+    set_2_list = list(set_2)
+    new_list = set(set_1_list) ^ set(set_2_list)
+    return new_list
