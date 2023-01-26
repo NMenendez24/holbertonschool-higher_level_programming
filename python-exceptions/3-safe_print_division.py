91f957f2c8170699c677a2052966b5a87154331d
NMenendez24 <nahuelmt24@gmail.com>
2023-01-26 14:05:58 -0300

    Tasks 3 and 4

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    if b == 0:
+        result = None
+    try:
+        result = a / b
+        print("Inside result: {:.2}".format(result))
+        return result
+    except:
+        print("Inside result: {}".format(result))
+        return None
