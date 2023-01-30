6a7866340073a24a592853642cdc9799ccf66214
NMenendez24 <nahuelmt24@gmail.com>
2023-01-30 17:07:51 -0300

    Tasks 1 and 2

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a square"""
+
+
+class Square:
+    """Define a square"""
+    __size = None
+    """Init"""
+    def __init__(self, size):
+        self.__size = size
---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define a square"""
+
+
+class Square:
+    """Define a square"""
+    __size = None
+    """Init"""
+    def __init__(self, size=0):
+        if size is None:
+            size = 0
+        else:
+            self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
