f6e033ec7d64187be51a1a9c5bb7d51138fef97c
NMenendez24 <nahuelmt24@gmail.com>
2023-01-31 14:33:55 -0300

    Task 6 Tuple Fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -11,15 +11,11 @@ class Square:
         if size is None:
             size = 0
         else:
-            self.__size = size
+            self.size = size
         if position is None:
             position = (0, 0)
         else:
-            self.__position = position
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+            self.position = position
 
     def area(self):
         """Returns the area of the square"""
@@ -39,24 +35,29 @@ class Square:
             raise TypeError("size must be >= 0")
         self.__size = value
 
-    def my_print(self):
-        if self.__size == 0:
-            print("")
-        if self.__size is not None and self.__position is not None:
-            for k in range(0, list(self.__position)[1]):
-                print("")
-            for i in range(0, self.__size):
-                for j in range(0, list(self.__position)[0]):
-                    print(" ",end="")
-                for k in range(0, self.__size):
-                    print("#", end="")
-                print("")
-
     @property
     def position(self):
         return self.__position
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or len(value) != 2:
+        if type(value) != tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        if self.__size is not None and self.__position is not None:
+            for k in range(0, self.__position[1]):
+                print("")
+            for i in range(0, self.__size):
+                for j in range(0, self.__position[0]):
+                    print(" ", end="")
+                for k in range(0, self.__size):
+                    print("#", end="")
+                print("")
