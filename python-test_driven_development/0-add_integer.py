4c17b32f082bcad8706f235ed8a2e2a958e9d773
NMenendez24 <nahuelmt24@gmail.com>
2023-02-02 10:49:45 -0300

    Test Task 0 and documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 
 
 def add_integer(a, b=98):
+    """Adds two integers"""
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     elif type(b) != int and type(b) != float:
