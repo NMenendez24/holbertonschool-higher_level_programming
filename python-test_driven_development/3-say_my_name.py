3975bd90f5a4d2ee573f6e2576d1e6265a1bb339
NMenendez24 <nahuelmt24@gmail.com>
2023-02-02 12:28:13 -0300

    Task 3 Doc and test

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Writes My name is <first name> <last name>"""
+
+
 def say_my_name(first_name, last_name=""):
+    """Function that prints"""
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
