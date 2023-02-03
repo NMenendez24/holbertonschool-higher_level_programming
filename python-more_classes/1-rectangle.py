94342d6045e42020af5e9374dae94ce4e1384585
NMenendez24 <nahuelmt24@gmail.com>
2023-02-03 14:45:32 -0300

    Task 1 Fix

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -41,15 +41,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
-######
-
-
-Rectangle = __import__('1-rectangle').Rectangle
-
-my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.__dict__)
