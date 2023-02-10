aa0cbe77f211fe792acc4808ac567ccf75df1cc8
NMenendez24 <nahuelmt24@gmail.com>
2023-02-10 16:29:17 -0300

    Task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Define a class that inherits from BaseGeometry"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle Class"""
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator(self, "width", width)
+        self.__width = width
+        BaseGeometry.integer_validator(self, "height", height)
+        self.__height = height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__height * self.__width
+
+r = Rectangle(3, 5)
+
+print(r)
+print(r.area())
