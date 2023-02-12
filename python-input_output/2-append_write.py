97ecd85be224230d4ba8569f366ca133a4fe5ad3
NMenendez24 <nahuelmt24@gmail.com>
2023-02-12 12:51:07 -0300

    Task 2 Doc

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,5 +5,6 @@
 
 
 def append_write(filename="", text=""):
+    """Appends to a file and returns the caracters appended"""
     with open(filename, 'a', encoding='utf-8') as f:
         return f.write(text)
