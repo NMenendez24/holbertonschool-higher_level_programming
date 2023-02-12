100df6688d42287e49e47e146a0cda5eb495f2d8
NMenendez24 <nahuelmt24@gmail.com>
2023-02-12 14:49:12 -0300

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """adds all arguments to a Python list, and then save them to a file"""
 
-
 save_to_json_file = __import__('5-save_to_json').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 from sys import argv
-
 
 
 obj_list = []
