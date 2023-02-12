0c5745c59e201558d945f76b51fb419facff5158
NMenendez24 <nahuelmt24@gmail.com>
2023-02-12 13:00:26 -0300

    Task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""Returns the representation of a JSON object"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """Returns the json representation of an objet"""
+    return json.dumps(my_obj)
