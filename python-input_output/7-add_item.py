5e0e26ea1c9d3f7bd28a523a61063d1b793e25cf
NMenendez24 <nahuelmt24@gmail.com>
2023-02-12 16:56:03 -0300

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,12 +6,16 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
 import os
 
+def main():
+    """Function"""
+    if os.path.exists('add_item.json'):
+        obj_list = load_from_json_file('add_item.json')
+    else:
+        obj_list = []
+    for i in range(1, len (sys.argv)):
+        """Test doc"""
+        obj_list.append(sys.argv[i])
+    save_to_json_file(obj_list, 'add_item.json')
 
-if os.path.exists('add_item.json'):
-    obj_list = load_from_json_file('add_item.json')
-else:
-    obj_list = []
-for i in range(1, len (sys.argv)):
-    """Test doc"""
-    obj_list.append(sys.argv[i])
-save_to_json_file(obj_list, 'add_item.json')
+if __name__ == '__main__':
+    main()
