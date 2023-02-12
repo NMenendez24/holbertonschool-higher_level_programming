128d8720b44a7d7b2bad794ddb5d279d422300b5
NMenendez24 <nahuelmt24@gmail.com>
2023-02-12 14:50:01 -0300

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""adds all arguments to a Python list, and then save them to a file"""
+"""Module adds all arguments to a Python list, and then save them to a file"""
 
 save_to_json_file = __import__('5-save_to_json').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
