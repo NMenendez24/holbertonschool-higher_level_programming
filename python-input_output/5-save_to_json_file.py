f452cbb1d50aa55c4479d314944034cff4c95ab0
NMenendez24 <nahuelmt24@gmail.com>
2023-02-12 16:32:01 -0300

    Some function changes

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,5 +9,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Writes to a file"""
-    with open(filename, 'w') as f:
+    with open(filename, mode='w', encoding='utf-8') as f:
         f.write(json.dumps(my_obj))
--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -9,5 +9,5 @@ import json
 
 def load_from_json_file(filename):
     """creates a object from a file"""
-    with open(filename, 'r') as f:
+    with open(filename, mode='r', encoding='utf-8') as f:
         return json.load(f)
