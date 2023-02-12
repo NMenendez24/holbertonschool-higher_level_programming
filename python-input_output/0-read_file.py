74d8496dbf44b9c46813b49cffd90e91b674db70
NMenendez24 <nahuelmt24@gmail.com>
2023-02-12 12:32:34 -0300

    Task 1 Doc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+
+"""Reads a file and prints to stdout"""
+
+
 def read_file(filename=""):
+    """Opens the file and prints"""
     with open(filename, encoding='utf-8') as f:
         print(f.read())
