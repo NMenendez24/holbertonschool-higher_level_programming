acee3445542605bcde42a507aaf3fa289e2a7c58
NMenendez24 <nahuelmt24@gmail.com>
2023-02-15 18:35:05 -0300

    Tasks 8 and 9

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -96,16 +96,19 @@ class Rectangle(Base):
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
                 self.__y, self.__width, self.__height))
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Updates the values"""
         if args:
-            if len(args) > 0:
+            if len(args) == 1:
                 self.id = args[0]
-            if len(args) > 1:
+            if len(args) == 2:
                 self.__width = args[1]
-            if len(args) > 2:
+            if len(args) == 3:
                 self.__height = args[2]
-            if len(args) > 3:
+            if len(args) == 4:
                 self.__x = args[3]
-            if len(args) > 4:
+            if len(args) == 5:
                 self.__y = args[4]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
