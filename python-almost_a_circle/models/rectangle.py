399ac66b76ec69f38734eb5a9c67e68d969ff2d5
NMenendez24 <nahuelmt24@gmail.com>
2023-02-15 17:03:02 -0300

    Task 6 PEP8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -89,5 +89,5 @@ class Rectangle(Base):
 
     def __str__(self) -> str:
         """Str method"""
-        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, \
-            self.__y, self.__width, self.__height))
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                self.__y, self.__width, self.__height))
