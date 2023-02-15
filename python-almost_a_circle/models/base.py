ac652ec58b1c166819b7d11bced932148ed76cdd
NMenendez24 <nahuelmt24@gmail.com>
2023-02-15 19:54:57 -0300

    Task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """Define a class Base"""
 
 
+import json
+
+
 class Base:
     """Base Class"""
     __nb_objects = 0
@@ -12,3 +15,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON representation of a list of dicts"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return []
+        else:
+            return json.dumps(list_dictionaries)
