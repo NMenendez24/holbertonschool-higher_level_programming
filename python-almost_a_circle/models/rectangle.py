5d1ef820e1e7da64ccbdc822babde726b200bd34
NMenendez24 <nahuelmt24@gmail.com>
2023-02-15 19:44:58 -0300

    Task 13

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -112,3 +112,7 @@ class Rectangle(Base):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {'id': self.id, 'width': self.__width,
+                'height': self.__height, 'x': self.__x, 'y': self.__y}
