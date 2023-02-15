945e8a7f3ce6ab1e100d0036929fe8b324ab3a11
NMenendez24 <nahuelmt24@gmail.com>
2023-02-15 20:43:10 -0300

    Task 16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,17 +19,24 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON representation of a list of dicts"""
-        if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
-
+    """
     @classmethod
     def save_to_file(cls, list_objs):
+        dict_objs = []
         dict_objs = [lists.to_dictionary() for lists in list_objs]
         with open("{}.json".format(cls.__name__), mode='w',
                 encoding='utf-8') as f:
             f.write(cls.to_json_string(list_objs))
-        if dict_objs == []:
-            return []
-        return dict_objs
+        """
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes the JSON string representation of list_objs to file"""
+        dict_objs = []
+        dict_objs = [lists.to_dictionary() for lists in list_objs]
+        with open(f"{cls.__name__}.json", mode='w', encoding='utf-8') as f:
+            f.write(cls.to_json_string(dict_objs))
+
