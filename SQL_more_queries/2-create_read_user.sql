c5453bd398033b230e21a849893eb29f6e88bfe8
NMenendez24 <nahuelmt24@gmail.com>
2023-03-15 13:02:07 -0300

    Task 2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Creates a user with select permision
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
