653a1101b615eece7c5551c3c2fa3437b38144a2
NMenendez24 <nahuelmt24@gmail.com>
2023-03-15 18:54:34 -0300

    Task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Lists all the cities and their State
-SELECT DISTINCT cities.id, cities.name, states.name FROM cities JOIN states ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states WHERE cities.state_id = states.id ORDER BY cities.id ASC;
