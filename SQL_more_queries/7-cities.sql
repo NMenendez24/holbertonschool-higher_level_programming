b9f256eb3e3467326cf337c45d20172c853e3301
NMenendez24 <nahuelmt24@gmail.com>
2023-03-15 15:10:52 -0300

    Task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,9 @@
 -- Creates a database and a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
     state_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id),
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
     name VARCHAR(256) NOT NULL
 );
