b556b504dcca2f6aebd5b9c37512bf7f58563d27
NMenendez24 <nahuelmt24@gmail.com>
2023-03-15 15:17:27 -0300

    Task 7 Fix?

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,10 @@
 -- Creates a database and a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE UNSIGNED NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(id),
     state_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+    FOREIGN KEY(state_id) REFERENCES states(id),
     name VARCHAR(256) NOT NULL
 );
