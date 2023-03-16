eb1650683747053cad2e0361d9cda76b0aa2245e
root <root@H151.hbtn.io>
2023-03-16 09:50:31 -0300

    Task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres WHERE tv_show_genres.genre_id = tv_shows.id ORDER BY tv_shows.title and tv_show_genres.id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres JOIN tv_shows WHERE tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
