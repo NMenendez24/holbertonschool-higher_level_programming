d410617e2055283304f92339b5f0237a3292b79e
NMenendez24 <nahuelmt24@gmail.com>
2023-03-17 16:56:18 -0300

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Lists the values of a table"""
+
+
+def select_states():
+    """List all the states in the states table"""
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM states WHERE states.name = '{argv[4]}'")
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    db.close()
+
+
+if __name__ == "__main__":
+    select_states()
