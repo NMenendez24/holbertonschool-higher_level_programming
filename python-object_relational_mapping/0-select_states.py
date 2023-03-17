d90802dc15e9c3cde73165c6a7eb920f18c1320b
NMenendez24 <nahuelmt24@gmail.com>
2023-03-17 16:44:19 -0300

    Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,16 +2,21 @@
 """Lists the values of a table"""
 
 
-import MySQLdb
-from sys import argv
+def select_states():
+    """List all the states in the states table"""
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    db.close()
 
-db = MySQLdb.connect(host="localhost",
-                     user=argv[1],
-                     passwd=argv[2],
-                     db=argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states")
-rows = cur.fetchall()
-for i in rows:
-    print(i)
-db.close()
+
+if __name__ == "__main__":
+    select_states()
