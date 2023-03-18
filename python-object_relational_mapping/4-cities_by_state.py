3ec6ad6f9e4a2380af116cd03c238a7a5d9f8804
NMenendez24 <nahuelmt24@gmail.com>
2023-03-18 12:44:55 -0300

    Task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Lists the values of a table"""
+
+
+def select_states():
+    """List all the states in the states table"""
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+                FROM cities JOIN states \
+                WHERE cities.state_id = states.id ORDER BY cities.id ASC;")
+    rows = cur.fetchall()
+    print(rows)
+    for i in rows:
+        if i[1][0].isupper():
+            print(i)
+    db.close()
+
+
+if __name__ == "__main__":
+    select_states()
