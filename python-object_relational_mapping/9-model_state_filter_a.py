96b410bbfcfe31e4f97b103de8e672efb654d48e
NMenendez24 <nahuelmt24@gmail.com>
2023-03-19 18:21:32 -0300

    Task 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Fetch the columns of the states table"""
+
+from sys import argv
+from sqlalchemy import create_engine, text
+from model_state import Base, State
+
+
+def list_states():
+    """Fetch the table and prints it"""
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost:3306\
+                        /{}".format(argv[1], argv[2], argv[3]))
+    connect = eng.connect()
+    rows = connect.execute(text('SELECT * FROM states WHERE name LIKE "%a%"'))
+    for element in rows:
+        print(f"{element[0]}: {element[1]}")
+
+
+if __name__ == "__main__":
+    list_states()
