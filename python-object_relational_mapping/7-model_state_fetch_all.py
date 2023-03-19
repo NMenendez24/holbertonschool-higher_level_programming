94d5b89d1a9527013c15155145c95cc6065dc248
NMenendez24 <nahuelmt24@gmail.com>
2023-03-19 17:52:08 -0300

    Task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,15 +2,21 @@
 """Fetch the columns of the states table"""
 
 from sys import argv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from model_state import Base, State
 
 
 def list_states():
     """Fetch the table and prints it"""
     eng = create_engine("mysql+mysqldb://{}:{}@localhost:3306\
-                        /{}".format(argv[1], argv[2], argv[3]),
-                        pool_pre_ping=True)
+                        /{}".format(argv[1], argv[2], argv[3]))
     connect = eng.connect()
-    rows = connect.execute('SELECT * FROM states')
-    print(rows)
+    rows = connect.execute(text('SELECT * FROM states'))
+    number = 0
+    for element in rows:
+        number += 1
+        print(f"{number}: {element[0]}")
+
+
+if __name__ == "__main__":
+    list_states()
