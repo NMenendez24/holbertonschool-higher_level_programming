1d015dc5ea51e8c88fa7db61230e2f26720d2192
NMenendez24 <nahuelmt24@gmail.com>
2023-03-19 16:38:37 -0300

    Task 7

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Doc"""
+
+
+import sqlalchemy as sqla
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    id = sqla.Column(sqla.Integer, primary_key=True, nullable=False)
+    name = sqla.Column(sqla.String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    pass
