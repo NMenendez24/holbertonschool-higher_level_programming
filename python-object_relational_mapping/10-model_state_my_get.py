f4164a25db6756743c4faa3e13297004a7dc0c0f
NMenendez24 <nahuelmt24@gmail.com>
2023-03-19 21:57:04 -0300

    Task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Fetch the columns of the states table"""
+
+from sys import argv
+from sqlalchemy import create_engine, text
+from model_state import Base, State
+
+
+def list_states():
+    """Fetch the table and prints it"""
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost:3306\
+                        /{}".format(argv[1], argv[2], argv[3]))
+    connect = eng.connect()
+    rows = connect.execute(text('SELECT id FROM states WHERE name\
+                                 = "{}"'.format(argv[4])))
+    if rows is None:
+        print("Not Found")
+    else:
+        print(f"{rows[0]}")
+
+
+if __name__ == "__main__":
+    list_states()
