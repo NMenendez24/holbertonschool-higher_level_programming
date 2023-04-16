696b01b913df729381a4bae98a486c0ee07484fb
NMenendez24 <nahuelmt24@gmail.com>
2023-04-16 16:59:25 -0300

    Fix size

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,10 +4,10 @@ if (isNaN(arg[0])) {
   console.log('Missing size');
 } else {
   let row = '';
-  for (let j = 0; j < 5; j++) {
+  for (let j = 0; j < arg[0]; j++) {
     row += 'X';
   }
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < arg[0]; i++) {
     console.log(row);
   }
 }
