4efbd9d73355613e212e3ba38f35e017a4a82f0e
NMenendez24 <nahuelmt24@gmail.com>
2023-04-16 20:19:16 -0300

    Task 0 to 3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !w || !h) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let row = '';
+    for (let j = 0; j < this.width; j++) {
+      row += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+};
