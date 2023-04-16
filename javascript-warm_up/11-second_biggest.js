edd625fde093d5588537a55f97f0636b66a960d6
NMenendez24 <nahuelmt24@gmail.com>
2023-04-16 18:38:21 -0300

    Task 11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let arg = process.argv.slice(2);
+if (arg.length === 0 || arg.length === 1) {
+  console.log('0');
+} else {
+  arg = arg.sort((a, b) => a - b);
+  console.log(arg[arg.length - 2]);
+}
