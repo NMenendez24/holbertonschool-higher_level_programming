bf08bfe3024c618dc57755db6786aa49a1eb11c5
NMenendez24 <nahuelmt24@gmail.com>
2023-04-16 20:40:14 -0300

    Task 6

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -6,17 +6,4 @@ module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-  charPrint (c) {
-    let character = "X"
-    if (c) {
-        character = c; 
-    }
-    let row = '';
-    for (let j = 0; j < this.size; j++) {
-      row += character;
-    }
-    for (let i = 0; i < this.size; i++) {
-      console.log(row);
-    }
-  }
 };
---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let character = 'X';
+    if (c) {
+      character = c;
+    }
+    let row = '';
+    for (let j = 0; j < this.size; j++) {
+      row += character;
+    }
+    for (let i = 0; i < this.size; i++) {
+      console.log(row);
+    }
+  }
+};
