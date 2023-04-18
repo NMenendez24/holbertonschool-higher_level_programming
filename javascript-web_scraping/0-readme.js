69b4493f1351ebdb0d6a4a6586effb84179b0375
NMenendez24 <nahuelmt24@gmail.com>
2023-04-18 13:14:00 -0300

    Semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-const fs = require( 'fs' );
-let file = args[0]
-fs.readFile(file, 'utf-8', function(err, data) {if (err) {console.log(err)} else {console.log(data)}})
+const fs = require('fs');
+const file = args[0];
+fs.readFile(file, 'utf-8', function (err, data) { if (err) { console.log(err); } else { console.log(data); } });
