6609b07a6b63ef235efec6d00f52cd89b69b1df0
NMenendez24 <nahuelmt24@gmail.com>
2023-04-18 13:50:02 -0300

    semistandard

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,5 +2,5 @@
 const args = process.argv.slice(2);
 const fs = require('fs');
 const file = args[0];
-let string = args[1];
-fs.writeFile(file, string, function (err) {if (err) {console.log(err)}});
+const string = args[1];
+fs.writeFile(file, string, function (err) { if (err) { console.log(err); } });
