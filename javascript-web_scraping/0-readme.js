2b8ffc02d506cb6150293f3894c45f574e3f991c
NMenendez24 <nahuelmt24@gmail.com>
2023-04-18 13:13:30 -0300

    Task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,4 +2,4 @@
 const args = process.argv.slice(2);
 const fs = require( 'fs' );
 let file = args[0]
-fs.readFile(file, 'utf-8', function(err, data) {console.log(data)})
+fs.readFile(file, 'utf-8', function(err, data) {if (err) {console.log(err)} else {console.log(data)}})
