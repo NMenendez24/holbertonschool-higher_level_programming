01be46d9c80d2918a08ca858699276291f3faed9
NMenendez24 <nahuelmt24@gmail.com>
2023-04-19 16:31:10 -0300

    Task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + args[0];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(JSON.parse(body).title);
+});
