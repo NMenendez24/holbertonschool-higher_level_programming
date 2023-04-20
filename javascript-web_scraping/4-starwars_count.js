295a164275d346f3af4088fde11684a81b424c95
NMenendez24 <nahuelmt24@gmail.com>
2023-04-20 13:49:41 -0300

    Task 4 final

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
-const args = process.argv.slice(2);
-let counter = 0;
-const character = 'https://swapi-api.hbtn.io/api/people/18/';
-for (let i = 1; i <= 7; i++) {
-  const url = 'https://swapi-api.hbtn.io/api/films/' + i + '/';
-  counter += request(url, function (error, response, body) {
-    if (error) {
-      console.log(error);
-    }
-    if (JSON.parse(body).characters.includes(character)) {
-      return 1;
-    }
-  });
-}
-console.log(counter);
+request.get("https://swapi-api.alx-tools.com/api/people/18/", function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  console.log((JSON.parse(body).films.length))
+});
