12a724afda20fb038e8c36c17b05e875994dcda9
NMenendez24 <nahuelmt24@gmail.com>
2023-04-21 15:41:54 -0300

    Task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
+const fs = require('fs');
 const args = process.argv.slice(2);
 let url = args[0];
   request.get(url, function (error, response, body) {
     if (error) {
       console.log(error);
     }
-    console.log((body));
+    fs.writeFile(args[1], body, function (err) { if (err) { console.log(err); } });
   }
 );
