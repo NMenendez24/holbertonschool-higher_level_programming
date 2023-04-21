97becd38d942e957818856b5fe34f3c8bfc59e5f
NMenendez24 <nahuelmt24@gmail.com>
2023-04-21 15:43:34 -0300

    semistandard

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,11 +2,11 @@
 const request = require('request');
 const fs = require('fs');
 const args = process.argv.slice(2);
-let url = args[0];
-  request.get(url, function (error, response, body) {
-    if (error) {
-      console.log(error);
-    }
-    fs.writeFile(args[1], body, function (err) { if (err) { console.log(err); } });
+const url = args[0];
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
   }
+  fs.writeFile(args[1], body, function (err) { if (err) { console.log(err); } });
+}
 );
