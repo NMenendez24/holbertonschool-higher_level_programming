4a6baa1023f45f868bb4f2f5774ceeb7864f811c
NMenendez24 <nahuelmt24@gmail.com>
2023-04-21 19:40:57 -0300

    All tasks

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  data.results.forEach(movie => {
+    $('#list_movies').append('<li>' + movie.title + '</li>');
+  });
+});
